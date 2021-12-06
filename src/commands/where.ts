04bfb06f27117964de2a579cc3385cf5acfb7890
Mike Donnalley <mdonnalley@salesforce.com>
2021-12-06 13:29:17 -0700

    feat: allow . as arg to where cmd

--- src/commands/where.ts
+++ src/commands/where.ts
@@ -1,5 +1,6 @@
 import { Command, Flags } from '@oclif/core';
 import { Repos } from '../repos';
+import { parseRepoNameFromPath } from '../util';
 
 export default class View extends Command {
   public static description = 'Print location of a repository.';
@@ -19,8 +20,9 @@ export default class View extends Command {
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(View);
+    const repoName = (args.repo === '.' ? parseRepoNameFromPath() : args.repo) as string;
     const repos = await Repos.create();
-    const repo = repos.getOne(args.repo);
+    const repo = repos.getOne(repoName);
     this.log(flags.remote ? repo.urls.html : repo.location);
   }
 }
