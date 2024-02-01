2a926961e54ca8021965ec45b9767c1bcf84a145
Mike Donnalley <mdonnalley@salesforce.com>
2024-02-01 13:04:44 -0700

    fix: add untracked files

---
+++ src/config-file.ts
@@ -0,0 +1,105 @@
+import {AsyncOptionalCreatable} from '@salesforce/kit'
+import {Stats} from 'node:fs'
+import {access, readFile, stat, writeFile} from 'node:fs/promises'
+import os from 'node:os'
+import path from 'node:path'
+
+import {Directory} from './directory.js'
+
+export abstract class ConfigFile<T extends Record<string, unknown>> extends AsyncOptionalCreatable<string> {
+  public static MPM_DIR = path.join(os.homedir(), '.multi')
+
+  public filepath: string
+  public stats!: Stats
+
+  private contents!: T
+
+  public constructor(filename: string) {
+    super(filename)
+    this.filepath = path.join(ConfigFile.MPM_DIR, filename)
+  }
+
+  public entries(): Array<[keyof T, T[keyof T]]> {
+    return Object.entries(this.getContents()) as Array<[keyof T, T[keyof T]]>
+  }
+
+  public async exists(filepath = this.filepath): Promise<boolean> {
+    try {
+      await access(filepath)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  protected format(contents: T): string {
+    return JSON.stringify(contents, null, 2)
+  }
+
+  public get(key: keyof T): T[keyof T] {
+    return this.contents[key]
+  }
+
+  public getContents(): T {
+    return this.contents
+  }
+
+  public has(key: keyof T): boolean {
+    return this.keys().includes(key)
+  }
+
+  protected async init(): Promise<void> {
+    await Directory.create({name: ConfigFile.MPM_DIR})
+    if (await this.exists()) {
+      this.contents = await this.read()
+    } else {
+      this.contents = this.make()
+      await this.write()
+    }
+
+    this.stats = await stat(this.filepath)
+  }
+
+  public keys(): Array<keyof T> {
+    return Object.keys(this.getContents()) as Array<keyof T>
+  }
+
+  protected make(): T {
+    return {} as T
+  }
+
+  protected parse(contents: string): T {
+    return JSON.parse(contents) as T
+  }
+
+  public async read(): Promise<T> {
+    this.contents = this.parse(await readFile(this.filepath, 'utf8'))
+    return this.contents
+  }
+
+  public set(key: keyof T, value: T[keyof T]): void {
+    this.contents[key] = value
+  }
+
+  public unset(key: keyof T): void {
+    delete this.contents[key]
+  }
+
+  public update(key: keyof T, value: T[keyof T]): void {
+    if (typeof this.contents[key] === 'string' && typeof value === 'string') {
+      this.contents[key] = value
+      return
+    }
+
+    // @ts-expect-error because we know that value is an object at this point.
+    this.contents[key] = {...this.contents[key], ...value}
+  }
+
+  public values(): Array<T[keyof T]> {
+    return Object.values(this.getContents()) as Array<T[keyof T]>
+  }
+
+  public async write(newContents: T = this.contents): Promise<void> {
+    await writeFile(this.filepath, this.format(newContents))
+  }
+}
---
+++ src/multi-wrapper.ts
@@ -0,0 +1,54 @@
+import {AsyncOptionalCreatable} from '@salesforce/kit'
+import {writeFile} from 'node:fs/promises'
+import path from 'node:path'
+
+import {ConfigFile} from './config-file.js'
+import {Tasks} from './tasks.js'
+import {ZshRc} from './zsh-rc.js'
+
+const TEMPLATE = `#!/usr/bin/env bash
+
+function _multi {
+  local tasks=$(sed -e 's/\:.*//;s/ .*//' @TASKS_PATH@ | tr '\\n' ' ')
+  local multi_exec=$(which -p multi)
+
+  if [[ " $tasks " =~ .*\\ $1\\ .* ]]; then
+    if [[ " $@ " =~ .*\\ --help\\ .* ]]; then
+      echo "--help is not supported on tasks"
+    else
+      if [[ -n \${@:2} ]]; then
+        source <($multi_exec task get $1) \${@:2}
+      else
+        source <($multi_exec task get $1) ""
+      fi
+    fi
+  elif [[ "$1" == "cd" && "$2" != "--help" ]]; then
+    cd $(eval $multi_exec where $2)
+  else
+    eval $multi_exec "$@"
+  fi
+}
+
+alias multi='_multi'
+alias m='_multi'
+`
+
+/**
+ * We wrap the `multi` executable for two reasons:
+ * 1. To support executing user defined tasks
+ * 2. To support the `cd` command. Node can't change the directory
+ * of the executing shell, so we have to do it in bash.
+ */
+export class MultiWrapper extends AsyncOptionalCreatable {
+  public static FILE_PATH = path.join(ConfigFile.MPM_DIR, 'multi-wrapper.bash')
+
+  protected async init(): Promise<void> {
+    if (process.platform === 'win32') return
+    const tasks = await Tasks.create()
+    const contents = TEMPLATE.replace('@TASKS_PATH@', tasks.filepath)
+    await writeFile(MultiWrapper.FILE_PATH, contents)
+    const zshRc = await ZshRc.create()
+    zshRc.append(`[[ -r ${MultiWrapper.FILE_PATH} ]] && source ${MultiWrapper.FILE_PATH}`)
+    await zshRc.write()
+  }
+}
