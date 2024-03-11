9e5e8cacfb66729d395037f392a313fa149e2c2a
Mike Donnalley <mdonnalley@salesforce.com>
2024-03-11 15:15:14 -0600

    feat: add org overview command

---
+++ src/commands/org/overview.ts
@@ -0,0 +1,123 @@
+/* eslint-disable perfectionist/sort-objects */
+import {Args, Command, Flags, ux} from '@oclif/core'
+import chalk from 'chalk'
+import sortBy from 'lodash.sortby'
+import {match} from 'minimatch'
+
+import {Github} from '../../github.js'
+import {Repos} from '../../repos.js'
+
+export default class Overview extends Command {
+  public static args = {
+    org: Args.string({description: 'Github org', required: true}),
+  }
+
+  static description =
+    'Provides issue, pull request, and discussion counts for the request repositories. Requires GH_TOKEN to be set in the environment.'
+
+  static enableJsonFlag = true
+
+  static examples = [
+    {
+      description: 'Get an overview of the issues and PRs for an org',
+      command: '<%= config.bin %> <%= command.id %> my-github-org',
+    },
+    {
+      description: 'Get an overview of the issues, PRs, and discussions for an org',
+      command: '<%= config.bin %> <%= command.id %> my-github-org --discussions',
+    },
+    {
+      description: 'Filter out repositories by minimatch pattern',
+      command: '<%= config.bin %> <%= command.id %> my-github-org --filter "my-repo-*"',
+    },
+  ]
+
+  static flags = {
+    discussions: Flags.boolean({
+      char: 'd',
+      description: 'Include discussions',
+    }),
+    'sort-by': Flags.option({
+      description: 'Sort by',
+      options: ['repo', 'issues', 'pulls'] as const,
+      default: 'repo',
+      char: 'b',
+    })(),
+    filter: Flags.string({
+      char: 'f',
+      description: 'Filter out repositories by minimatch pattern',
+      multiple: true,
+    }),
+  }
+
+  public async run(): Promise<Record<string, {discussions: number; issues: number; pulls: number}>> {
+    const {args, flags} = await this.parse(Overview)
+
+    const repos = await new Repos().init()
+    const github = new Github()
+
+    let filtered = repos.getReposOfOrg(args.org)
+    for (const filter of flags.filter ?? []) {
+      const matches = match(
+        filtered.map((r) => r.name),
+        filter,
+      )
+      filtered = filtered.filter((r) => matches.includes(r.name))
+    }
+
+    const [discussions, issues, pulls] = await Promise.all([
+      flags.discussions ? await github.repoDiscussions(filtered) : [],
+      await github.repoIssues(filtered),
+      await github.repoPulls(filtered),
+    ])
+
+    const results = Object.fromEntries(
+      filtered.map((repo) => [
+        repo.name,
+        {
+          discussions: discussions.filter((d) => d.repo === repo.name).length,
+          issues: issues.filter((i) => i.repo === repo.name).length,
+          pulls: pulls.filter((p) => p.repo === repo.name).length,
+        },
+      ]),
+    )
+
+    const totals = {
+      repo: 'totals',
+      discussions: discussions.length,
+      issues: issues.length,
+      pulls: pulls.length,
+    }
+
+    if (!this.jsonEnabled()) {
+      ux.table(
+        [
+          ...sortBy(
+            Object.entries(results).map(([repo, {discussions, issues, pulls}]) => ({
+              discussions,
+              issues,
+              pulls,
+              repo,
+            })),
+            flags['sort-by'],
+          ).reverse(),
+          totals,
+        ],
+        {
+          repository: {
+            header: 'Repository',
+            get: (row) => (row.repo === 'totals' ? chalk.bold('totals') : row.repo),
+          },
+          issues: {header: 'Issues'},
+          pulls: {header: 'PRs'},
+          ...(flags.discussions ? {discussions: {header: 'Discussions'}} : {}),
+        },
+        {
+          title: 'Overview',
+        },
+      )
+    }
+
+    return results
+  }
+}
