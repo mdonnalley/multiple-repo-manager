1b69852e2c61188ce2a17c1c5cda1c3ca331afcc
Mike Donnalley <mdonnalley@salesforce.com>
2024-03-15 12:36:55 -0600

    fix: unique keys for help output

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,12 +1,17 @@
 import {Box, Text} from 'ink'
+import crypto from 'node:crypto'
 import React from 'react'
+
+function createHash(input: string, index: number): string {
+  return crypto.createHash('sha256').update(input).digest('hex') + `_${index}`
+}
 
 export default function Help({helpOutput}: {readonly helpOutput: string[]}) {
   if (helpOutput.length === 0) return
   return (
     <Box borderColor="green" borderStyle="round" flexDirection="column" padding={2} paddingTop={0}>
-      {helpOutput.map((s) => (
-        <Text key={s}>{s}</Text>
+      {helpOutput.map((s, idx) => (
+        <Text key={createHash(s, idx)}>{s}</Text>
       ))}
     </Box>
   )
